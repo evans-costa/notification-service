628a50f521ea0b09a991979ee5a72ac78384a89c
Evandro <evandro.souzac@gmail.com>
2022-12-16 20:31:41 -0300

    creating notification and content tests

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,53 +1,66 @@
-import { Content } from "./content";
+import { Content } from './content';
+import { Replace } from 'src/helpers/Replace'
 
 export interface NotificationProps {
-    recipientId: string;
-    content: Content;
-    category: string;
-    readAt?: Date | null;
-    createdAt: Date;
+  recipientId: string;
+  content: Content;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
 }
 
 export class Notification {
-    private props: NotificationProps
+  private props: NotificationProps;
 
-    constructor(props: NotificationProps) {
-        this.props = props;
-    }
+  constructor(props: Replace<NotificationProps, {createdAt?: Date}>) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date()
+    };
+  }
 
-    public set recipientId (recipientId : string) { // setter
-        this.props.recipientId = recipientId;
-    }
+  public set recipientId(recipientId: string) {
+    // setter
+    this.props.recipientId = recipientId;
+  }
 
-    public get recipientId(): string { // getter
-        return this.props.recipientId;
-    }
+  public get recipientId(): string {
+    // getter
+    return this.props.recipientId;
+  }
+
+  public set content(content: Content) {
     
-    public set content (content : Content) { // setter
-        this.props.content = content;
-    }
+    this.props.content = content;
+  }
 
-    public get content(): Content { // getter
-        return this.props.content;
-    }
+  public get content(): Content {
+
+    return this.props.content;
+  }
+
+  public set category(category: string) {
+
+    this.props.category = category;
+  }
+
+  public get category(): string {
     
-    public set category (category : string) { // setter
-        this.props.category = category;
-    }
+    return this.props.category;
+  }
 
-    public get category(): string { // getter
-        return this.props.category;
-    }
+  public set readAt(readAt: Date | null | undefined) {
+    
+    this.props.readAt = readAt;
+  }
 
-    public set readAt (readAt : Date | null | undefined) { // setter
-        this.props.readAt = readAt;
-    }
+  public get readAt(): Date | null | undefined {
+    
+    return this.props.readAt;
+  }
 
-    public get readAt(): Date | null | undefined { // getter
-        return this.props.readAt;
-    }
-
-    public get createdAt(): Date { // getter
-        return this.props.createdAt;
-    }
+  public get createdAt(): Date {
+    
+    return this.props.createdAt;
+  }
 }
